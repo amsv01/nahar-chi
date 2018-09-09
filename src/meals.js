2f53f3c1d37c1270b2875c00c7a55da445262b43
Amin Mousavi <tigmin@gmail.com>
2018-09-09 20:24:29 +0200

    complete list of meals

--- src/meals.js
+++ src/meals.js
@@ -1,11 +1,40 @@
+const TAG_MEAT = 'گوشت قرمز';
+const TAG_CHICKEN = 'گوشت مرغ';
+const TAG_PULLED_MEAT = 'گوشت چرخ کرده';
+const TAG_EGGPLANT = 'بادمجان';
+const TAG_MUSHROOM = 'قارچ';
+const EGG = 'تخم مرغ';
+
 export default [
-    {name: 'قرمه سبزی', tags: ['گوشت قرمز']},
-    {name: 'قیمه', tags: ['گوشت قرمز']},
-    {name: 'خرش بادمجان', tags: ['گوشت قرمز']},
-    {name: 'مرغ ربی', tags: ['مرغ']},
-    {name: 'مرغ زعفرانی', tags: ['مرغ']},
-    {name: 'مرغ و قارچ', tags: ['مرغ']},
-    {name: 'جوجه کباب ماستی', tags: ['مرغ']},
-    {name: 'عدس پلو', tags: ['گوشت قرمز']},
-    {name: 'باقالی پلو', tags: ['گوشت قرمز']},
+    {name: 'قرمه سبزی', tags: [TAG_MEAT]},
+    {name: 'قیمه', tags: [TAG_MEAT]},
+    {name: 'خورش بادمجان', tags: [TAG_MEAT, TAG_EGGPLANT]},
+    {name: 'مرغ ربی', tags: [TAG_CHICKEN]},
+    {name: 'مرغ زعفرانی', tags: [TAG_CHICKEN]},
+    {name: 'مرغ و قارچ', tags: [TAG_CHICKEN, TAG_MUSHROOM]},
+    {name: 'جوجه کباب ماستی', tags: [TAG_CHICKEN]},
+    {name: 'عدس پلو', tags: [TAG_PULLED_MEAT]},
+    {name: 'باقالی پلو', tags: [TAG_MEAT]},
+    {name: 'خورش کرفس', tags: [TAG_MEAT, TAG_CHICKEN]},
+    {name: 'راگو', tags: [TAG_MEAT]},
+    {name: 'ماکارونی', tags: [TAG_PULLED_MEAT]},
+    {name: 'لوبیا پلو', tags: [TAG_PULLED_MEAT]},
+    {name: 'استانبولی', tags: [TAG_PULLED_MEAT]},
+    {name: 'میرزاقاسمی', tags: [TAG_PULLED_MEAT, TAG_EGGPLANT, EGG]},
+    {name: 'کشک بادمجان', tags: [TAG_PULLED_MEAT, TAG_EGGPLANT]},
+    {name: 'کوکو سبزی', tags: [EGG]},
+    {name: 'کوکو سیب زمینی', tags: [EGG]},
+    {name: 'املت', tags: [EGG]},
+    {name: 'کباب مرغ تابه', tags: [TAG_CHICKEN]},
+    {name: 'کباب تابه', tags: [TAG_PULLED_MEAT]},
+    {name: 'کتلت', tags: [TAG_PULLED_MEAT]},
+    {name: 'کوفته', tags: [TAG_PULLED_MEAT]},
+    {name: 'دلمه', tags: [TAG_PULLED_MEAT]},
+    {name: 'پیتزا', tags: [TAG_PULLED_MEAT]},
+    {name: 'چلو گوشت', tags: [TAG_MEAT]},
+    {name: 'آب گوشت', tags: [TAG_MEAT]},
+    {name: 'بیف استراگانف', tags: [TAG_MEAT]},
+    {name: 'خورش اسفناج', tags: [TAG_MEAT]},
+    {name: 'سوپ', tags: [TAG_CHICKEN]},
+    {name: '', tags: []},
 ];
